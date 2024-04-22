5690c38f8c16c28cbc7bd018c568d7d94eed5351
Krzysztof Serafin <serakrzysztof@gmail.com>
2024-04-22 10:11:59 +0200

    18.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.taks.service.DbService
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,14 +13,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
-    @GetMapping
-    public List<TaskDto> getTasks() {
-        return new ArrayList<>();
-    }
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
 
     @GetMapping
-    public TaskDto getTask(Long taskId) {
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+    @Autowired
+    public TaskController(DbService service, TaskMapper taskMapper) {
+        this.service = service;
+        this.taskMapper = taskMapper;
+    }
+
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) {
         return new TaskDto(1L, "test title", "test_content");
     }
 
@@ -24,7 +42,7 @@ public class TaskController {
 
     }
 
-    @PutMapping
+    @PutMapping(value = "{taskId}")
     public TaskDto updateTask(TaskDto taskDto) {
         return new TaskDto(1L, "Edited test title", "Test content");
     }
