02e88c74d3d345281f92075236f18755172dd92e
serak <serakrzysztof@gmail.com>
2024-08-06 22:10:29 +0200

    30.2

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -7,6 +7,26 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 public class AdminConfig {
+
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Value("${info.company.goal}")
+    private String companyGoal;
+
+    @Value("${info.company.email}")
+    private String companyEmail;
+
+    @Value("${info.company.phone}")
+    private String companyPhone;
+
+    public String getCompanyDetails() {
+        return companyName + ", " + companyGoal + ", " + companyEmail + ", " + companyPhone;
+    }
 }
